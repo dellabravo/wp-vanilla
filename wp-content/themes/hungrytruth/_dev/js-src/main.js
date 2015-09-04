82ff488fd7c9241edf1c788b1f1e1f74eb370cbb
Della Baines <dellabravo@gmail.com>
2015-09-04 09:38:33 -0400

    compile theme style

--- wp-content/themes/hungrytruth/_dev/js-src/main.js
+++ wp-content/themes/hungrytruth/_dev/js-src/main.js
@@ -1,19 +1,5 @@
  $(function(){
 
-	// close alert with session cookie
-	if($.cookie('alert_closed') != 'theclosed'){
-		console.log("cookie not set");
-		$(".alert").slideDown();
-	} else {
-		$(".alert").hide();
-	}
-
-	$(".alert .close").click(function(){
-		$(".alert").slideUp("slow");
-		$.cookie('alert_closed', 'theclosed', { path: '/' });
-		console.log("cookie set");
-	});
-
  	// to check if is a mobile device
 	var isMobile = {
 	    Android: function() {
@@ -35,20 +21,6 @@
 	        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 	    }
 	};
-
-	//Lazy load images
-	if($(window).width() > 992)
-		$(".desktop.unveil").unveil(0, function() {
-		  $(this).load(function() {
-		    this.style.opacity = 1;
-		  });
-		});
-	else
-		$(".mobile.unveil").unveil(0, function() {
-		  $(this).load(function() {
-		    this.style.opacity = 1;
-		  });
-		});
 	
 
 	// FastClick
@@ -67,59 +39,6 @@
  		$('.mobile-nav-btn.fd-stack').toggle();
  		$('body').toggleClass('slide-left');
  	});
-
- 	//tile hover interactions: I WISH I COULD HAVE DONE THIS IN CSS!!!!
- 	$('.tile .media').hover(function(){
- 		$(this).next().find('h4 a').toggleClass('on');
- 	});
-
- 	$('.tile-content > h4 a').hover(function(){
- 		$(this).closest(".tile").toggleClass('on');
- 	});
-
-
- 	// Sticky Items
- 	$(".sidebar-container, .social_toolbar, .juicer-feed-container").stick_in_parent();
-
- 	// p tags with strong for Article pages
- 	$("p:has(strong)").css({
- 		"margin-top" : 10,
- 		"margin-bottom" : 15
- 	});
-
- 	// Overflow container Table for Article pages
- 	$(".content-container table").wrap("<div class='overflow-wrap'/>");
-
-
- 	//wrap in responsive wrapper
- 	$("iframe").wrap("<div class='frame-wrap'/>");
-
- 	//prevent sidescroll for full width dropdown menu
- 	$('#fdl-mininav-expand').hover(function(){
- 		$('body').css("overflow-x", "hidden");
- 	});
-
-
- 	//Masonry for ajax load more
-	var $container = $('#masonry-grid'); // our container
-	$container.masonry({
-      itemSelector: '.tile',
-      gutter: '.gutter-sizer',
-      transitionDuration: 0
-    });	
-	$.fn.almComplete = function(alm){ // Ajax Load More callback function
-		if($('#masonry-grid').length){	  
-		  $container.masonry('reloadItems'); // Reload masonry items oafter callback	  
-		  $container.imagesLoaded( function() { // When images are loaded, fire masonry again.
-		    $container.masonry();
-		  });	  
-		}
-		$("img.unveil").unveil(0, function() {
-			  $(this).load(function() {
-			    this.style.opacity = 1;
-			  });
-		});
-	};
 
 
 //header mobile 
